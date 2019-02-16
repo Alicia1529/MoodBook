3391b218ce76190ff81353bbd16b1ccc59eacd4c
Alicia <tl2180@nyu.edu>
2019-02-16 17:08:37 -0500

    upload pictures

--- image.py
+++ image.py
@@ -1,27 +1,35 @@
 import http.client, urllib.request, urllib.parse, urllib.error, base64
+import requests
+import imageHelper as helper
+exp = 1;
+num = 10;
+face_analysis = []
+emotion = []
+sentiment = []
+f =  open("face_analysis.txt", "w")
+f.write(str(num))
 
 subscription_key = '25e6e963140041639e8a9c9e49749237'
 assert subscription_key
 
 params = urllib.parse.urlencode({
     # Request parameters
-    'returnFaceId': 'true',
+    'returnFaceId': 'false',
     'returnFaceLandmarks': 'false',
-    'returnFaceAttributes': 'age,gender,smile,emotion',
+    'returnFaceAttributes': 'emotion',
 })
-
-
 emotion_recognition_url = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect?%s"%params
-
-# 'https://eastus.api.cognitive.microsoft.com/face/v1.0/detect'
-# "https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize"
-
-image_path = "Input/Images/1.png"
-image_data = open(image_path, "rb").read()
-
-import requests
 headers  = {'Ocp-Apim-Subscription-Key': '25e6e963140041639e8a9c9e49749237', "Content-Type": "application/octet-stream" }
-response = requests.post(emotion_recognition_url, headers=headers, data=image_data)
-response.raise_for_status()
-analysis = response.json()
-print(analysis)
+
+for i in range(exp):
+    for j in range(num):
+        image_path = "Input/Images/Part"+str(i+1)+'/'+str(j+1)+".jpg"
+        image_data = open(image_path, "rb").read()
+        response = requests.post(emotion_recognition_url, headers=headers, data=image_data)
+        response.raise_for_status()
+        analysis = response.json()
+        emotion.append(analysis[0]['faceAttributes']["emotion"])
+        sentiment.append(helper.sentiment(analysis[0]['faceAttributes']["emotion"]))
+face_analysis.append(emotion)
+face_analysis.append(sentiment)
+f.write(str(face_analysis))
